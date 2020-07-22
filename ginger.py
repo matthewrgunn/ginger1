61a382ee2cd56962807ffa716abc59c305344bc8
matthewrgunn <62288332+matthewrgunn@users.noreply.github.com>
2020-07-22 17:45:19 -0400

    Create ginger.py

---
+++ ginger.py
@@ -0,0 +1,9 @@
+class Ginger:
+    def __init__(self, mood, outfit):
+        self.mood = mood
+        self.outfit = outfit
+
+ginger1= Ginger("Angry", "Bowtie")
+ginger2= Ginger("Happy", "Buttons")
+
+print(ginger1.mood)
