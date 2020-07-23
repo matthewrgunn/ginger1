c43222c87a3930d74ae25cc1612eebeaec990415
matthewrgunn <62288332+matthewrgunn@users.noreply.github.com>
2020-07-23 14:15:24 -0400

    Update ginger.py

--- ginger.py
+++ ginger.py
@@ -6,7 +6,7 @@ class Profile:
         self.password = password
 
 user1 = Profile("Matt", "matthewrgunn@upperline.com", 43, "Chewie")
-user2 = Profile("Chewie", "chewie@upperline.com", 2, "_M@tt!"_)
+user2 = Profile("Chewie", "chewie@upperline.com", 2, "_M@tt!_")
 user3 = Profile("Maddie", "maddie@gmail.com", 10, "Dadisthebest")
 
 print(user1.email)
